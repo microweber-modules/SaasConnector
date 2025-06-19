24b149c00772d2ea0503c4e5f8d05b8c6e5d7dc1
Peter Ivanov <peter@microweber.com>
2025-06-19 10:59:45 +0300

    update

--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['admin'])
-    ->name('admin.saas-connector.')
-    ->group(function () {
-        Route::get('/setup-wizard', [
-                \Modules\SaasConnector\Http\Controllers\Admin\SetupWizardController::class,
-                'index'
-            ]
-        )->name('setup-wizard');
-
-
-        Route::post('/install-template', [
-            \Modules\SaasConnector\Http\Controllers\Admin\SetupWizardController::class,
-            'installTemplate'])
-            ->name('install-template');
-    });
-
 Route::middleware(['xss', 'web'])
     ->name('saas-connector.')
     ->namespace('Modules\SaasConnector\Http\Controllers')
